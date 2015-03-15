d297569fd049a92962dc9f8e5d147066fc8c934d
kprince <461019665@qq.com>
2015-03-15 22:38:11 +0800

    Init deve;op

---
+++ app/src/com/xiaoming/random/model/Geo.java
@@ -0,0 +1,7 @@
+package com.xiaoming.random.model;
+
+/**
+ * Created by XIAOM on 2015/3/14.
+ */
+public class Geo {
+}
---
+++ app/src/com/xiaoming/random/model/Tag.java
@@ -0,0 +1,7 @@
+package com.xiaoming.random.model;
+
+/**
+ * Created by XIAOM on 2015/3/15.
+ */
+public class Tag {
+}
